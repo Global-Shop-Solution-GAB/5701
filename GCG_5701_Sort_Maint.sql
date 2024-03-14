cb3d0d754d46eb41b74d46de42f7dc7c4183dfad
Service Web <sw@gssmail.com>
2024-03-14 13:58:34 -0500

    Commit changes in C:\Websites\Git\emarlow\5701\GCG_5701_Sort_Maint.sql

---
+++ GCG_5701_Sort_Maint.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_5701_Sort_Maint" (
+ "Sort_Code" CHAR(10),
+ "Sort_Year" CHAR(4),
+ "Prefix" CHAR(4),
+ "Min_Seq" CHAR(6),
+ "Max_Seq" CHAR(6),
+ "Last_Chg_By" CHAR(8),
+ "Last_Sort_Serial" CHAR(30) );
